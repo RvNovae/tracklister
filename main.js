03d95b8b94c3efdd29a1900ce204ec0a16673ce9
RvNovae <kogel.oliver@web.de>
2019-07-17 20:07:55 -0400

    remove unused ipc declaration

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain} = require('electron')
+const {app, BrowserWindow } = require('electron')
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
