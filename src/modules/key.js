2dbf4480679d580f90ff4b1d9bd135252cb76752
Oliver Kogel <kogel.oliver@web.de>
2020-01-18 14:26:33 +0100

    more consistent shortcut support

--- src/modules/key.js
+++ src/modules/key.js
@@ -8,10 +8,15 @@ document.addEventListener('keyup', function(e) {
         });
     }
     if (e.key === "Enter") {
+        let counter = 0;
         Array.from(document.getElementsByClassName('modal')).forEach(function(elem) {
             if (elem.classList.contains('is-active')) {
+                counter++;
                 elem.getElementsByClassName('submit')[0].click();
             }
         });
+        if (counter == 0) {
+            Editor.Add.Append('new');
+        }
     }
 });
